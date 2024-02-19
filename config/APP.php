19fa985a8ebce56a9f152a42362124182e8162a1
YESEDGU <yesideduardoguerrero@hotmail.com>
2024-02-19 00:18:35 -0500

    Actualizacion Proyecto Crud_Ivetarios

---
+++ config/APP.php
@@ -0,0 +1,7 @@
+<?php
+    const SERVER="http://localhost/Crud_Inventarios/";
+
+    const COMPANY="Crud Inventarios";
+
+    date_default_timezone_set("America/Bogota");
+
---
+++ config/SERVER.php
@@ -0,0 +1,12 @@
+<?php
+
+    const SERVER="localhost";
+    const DB="crud_inventario";
+    const USER="";
+    const PASS="";
+
+    const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+    const METHOD="AES-256-CBC";
+    const SECRET_KEY='$CRUD_INVENTARIO@2024';
+    const SECRET_IV='012345'; 
