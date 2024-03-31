fb4ccfa1f0dec3713fa1924d2c383c7012d5f38e
YESEDGU <yesideduardoguerrero@hotmail.com>
2024-03-31 01:11:52 -0500

    Correccion nombre Sucursales en la barra de Navegacion

--- view/inc/NavLateral.php
+++ view/inc/NavLateral.php
@@ -82,7 +82,7 @@
 						</li>
 
 						<li>
-							<a href="company.html"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Empresa</a>
+							<a href="company.html"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Sucursales</a>
 						</li>
 					</ul>
 				</nav>
